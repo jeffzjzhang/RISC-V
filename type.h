fe0fd4bb5b272e03cbee75f2e53ec58b95c9d483
0900904A <Mzijiezhang@outlook.com>
2017-12-16 22:57:34 +0800

    add source code

--- type.h
+++ type.h
@@ -38,3 +38,74 @@ typedef enum {
 /* total size of memory space */
 #define MEMORY_SIZE (1024 * 1024)
 
+/* instruction structure defines all the types required by RISC-V ISA:
+     R -type: [ funct7 | rs2 | rs1 | funct3 | rd | opcode ]
+     I -type: [ imm[11:0] | rs1 | funct3 | rd | opcode ]
+     S -type: [ imm[11:5] | rs2 | rs1 | funct3 | imm[4:0] | opcode ]
+     SB-type: [ imm[12|10:5] | rs2 | rs1 | funct3 | imm[4:1|11] | opcode ]
+     U -type: [ imm[31:12] | rd | opcode ]
+     UJ-type: [ imm[20|10:1|11|19:12] | rd | opcode ]
+   For details, please refer to the RISC-V green card.  
+   also add some extra structure to simplify the codes */
+typedef union {
+    /* simpler to access the opcode */
+    struct {
+        unsigned int opcode : 7;
+        unsigned int        : 25;
+    };
+
+    /* just for debug */
+    Double bits;
+
+    /* R-type */
+    struct {
+        unsigned int opcode : 7;
+        unsigned int rd     : 5;
+        unsigned int funct3 : 3;
+        unsigned int rs1    : 5;
+        unsigned int rs2    : 5;
+        unsigned int funct7 : 7;
+    } rtype;
+
+    /* I-type */
+    struct {
+        unsigned int opcode : 7;
+        unsigned int rd     : 5;
+        unsigned int funct3 : 3;
+        unsigned int rs1    : 5;
+        unsigned int imm20  : 12;
+    } itype;
+
+    /* S-type */
+    struct {
+        unsigned int opcode : 7;
+        unsigned int imm5   : 5;
+        unsigned int funct3 : 3;
+        unsigned int rs1    : 5;
+        unsigned int rs2    : 5;
+        unsigned int imm7   : 7;
+    } stype;
+
+    /* SB-type */
+    struct {
+        unsigned int opcode : 7;
+        unsigned int imm5   : 5;
+        unsigned int funct3 : 3;
+        unsigned int rs1    : 5;
+        unsigned int rs2    : 5;
+        unsigned int imm7   : 7;
+    } sbtype;
+
+    /* U-type */
+    struct {
+        unsigned int opcode : 7;
+        unsigned int rd     : 5;
+        unsigned int imm20  : 20;
+    } utype;
+
+    struct {
+        unsigned int opcode : 7;
+        unsigned int rd     : 5;
+        unsigned int imm20  : 20;
+    } ujtype;
+} Instruction;   
